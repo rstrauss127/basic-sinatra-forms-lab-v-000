72e051aa95854adab1e1706abeb992575aaced57
Rebecca Strauss <rstrauss127@gmail.com>
2018-11-02 23:21:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,12 @@ class App < Sinatra::Base
 
   post '/team' do
     @team_name = params["name"]
-
+    @coach = params["coach"]
+    @point_guard = params["pg"]
+    @shooting_guard = params["sg"]
+    @power_fwd = params["pf"]
+    @small_fwd = params["sf"]
+    @center = params["c"]
     erb :team
   end
 end
